6f3bbfc4290074825e870554d5435a3eaa715a02
xs-web-lyq <3235951041@qq.com>
2022-08-27 18:49:38 +0800

    面试题 03.04. 化栈为队

---
+++ Queue/面试题 03.04. 化栈为队.js
@@ -0,0 +1,58 @@
+/**
+ * Initialize your data structure here.
+ */
+ var MyQueue = function() {
+    this.stack1 = []
+    this.stack2 = []
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack2.push(x)
+    return ;
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.stack1.length) return this.stack1.pop()
+    while(this.stack2.length){
+        this.stack1.push(this.stack2.pop())
+    }
+    return this.stack1.pop()
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if(this.stack1.length) return this.stack1[this.stack1.length - 1]
+    while(this.stack2.length){
+        this.stack1.push(this.stack2.pop())
+    }
+    return this.stack1[this.stack1.length - 1]
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return !this.stack1.length && !this.stack2.length;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
